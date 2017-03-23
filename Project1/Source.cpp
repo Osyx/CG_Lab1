66502555b46ad4c039e0f3280bfa3cb3a6ef2c19
Osyx <ofalkman@gmail.com>
2017-03-23 20:41:29 +0100

    Added static star canvas.

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -3,28 +3,39 @@
 #include <glm/glm.hpp>
 #include <vector>
 #include "SDLauxiliary.h"
+#include <ctime>
+#include <math.h>
 
 using namespace std;
 using glm::vec3;
+using glm::vec2;
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+const int F = SCREEN_HEIGHT / 2;
 SDL_Surface* screen;
+vector<vec3> stars(1000);
 
-void startRainbow();
+void update();
+void initStars();
+void initRainbow();
 void rainbowDraw(vector<vec3> left, vector<vec3> right);
 void interpolate(float a, float b, vector<float>& result);
 void interpolate(vec3 a, vec3 b, vector<vec3>& result);
 void testInterpolate();
+void starsDraw();
+vec2 conv3Dto2D(vec3 point);
 
 int main(int argc, char* argv[]) {
+	screen = InitializeSDL(SCREEN_WIDTH, SCREEN_HEIGHT);
 	testInterpolate();
-	startRainbow();
+	initRainbow();
+	initStars();
 	
 	return 0;
 }
 
-void startRainbow() {
+void initRainbow() {
 	vec3 topLeft(1, 0, 0);
 	vec3 topRight(0, 0, 1);
 	vec3 bottomLeft(0, 1, 0);
@@ -34,22 +45,22 @@ void startRainbow() {
 	interpolate(topLeft, bottomLeft, leftSide);
 	interpolate(topRight, bottomRight, rightSide);
 
-	screen = InitializeSDL(SCREEN_WIDTH, SCREEN_HEIGHT);
 	while (NoQuitMessageSDL()) {
 		rainbowDraw(leftSide, rightSide);
 	}
-	SDL_SaveBMP(screen, "screenshot.bmp");
+	SDL_SaveBMP(screen, "rainbow.bmp");
 }
 
 void rainbowDraw(vector<vec3> left, vector<vec3> right) {
 
-	SDL_LockSurface(screen);
+	if (SDL_MUSTLOCK(screen))
+		SDL_LockSurface(screen);
 
-	for (int y = 0; y < SCREEN_HEIGHT; ++y) {
+	for (size_t y = 0; y < SCREEN_HEIGHT; ++y) {
 		vector<vec3> row(SCREEN_WIDTH);
 		interpolate(left[y], right[y], row);
 
-		for (int x = 0; x < SCREEN_WIDTH; ++x) {
+		for (size_t x = 0; x < SCREEN_WIDTH; ++x) {
 			PutPixelSDL(screen, x, y, row[x]);
 		}
 	}
@@ -93,17 +104,67 @@ void testInterpolate() {
 	cout << "Using float values:" << endl;
 	vector<float> resultFloat(10);
 	interpolate(5, 14, resultFloat);
-	for (int i = 0; i < resultFloat.size(); ++i)
+	for (size_t i = 0; i < resultFloat.size(); ++i)
 		cout << resultFloat[i] << " ";
 	cout << "\n\nUsing glm::vec3:" << endl;;
 	vector<vec3> result(4);
 	vec3 a(1, 4, 9.2);
 	vec3 b(4, 1, 9.8);
 	interpolate(a, b, result);
-	for (int i = 0; i<result.size(); ++i) {
+	for (size_t i = 0; i<result.size(); ++i) {
 		cout << "( "
 			<< result[i].x << ", "
 			<< result[i].y << ", "
 			<< result[i].z << " ) ";
 	}
+}
+
+void initStars() {
+	srand((unsigned int) time(nullptr));
+	for (size_t i = 0; i < stars.size(); ++i) {
+		switch (rand() % 2) {
+			case 0:
+				stars[i].x = float(rand()) / float(RAND_MAX) - 1.0f;				
+				break;
+			default:
+				stars[i].x = float(rand()) / float(RAND_MAX);
+				break;
+		}
+		switch (rand() % 2) {
+		case 0:
+			stars[i].y = float(rand()) / float(RAND_MAX) - 1.0f;
+			break;
+		default:
+			stars[i].y = float(rand()) / float(RAND_MAX);
+			break;
+		}
+		stars[i].z = float(rand()) / float(RAND_MAX);
+	}
+	while (NoQuitMessageSDL()) {
+		starsDraw();
+	}
+	SDL_SaveBMP(screen, "stars.bmp");
+}
+
+vec2 conv3Dto2D(vec3 point) {
+	vec2 flatPoint(F * point.x / point.z + SCREEN_WIDTH / 2, F * point.y / point.z + SCREEN_HEIGHT / 2);
+	return flatPoint;
+}
+
+void starsDraw() {
+	SDL_FillRect(screen, 0, 0);
+	if (SDL_MUSTLOCK(screen))
+		SDL_LockSurface(screen);
+	vec3 color(1, 1, 1);
+	for (size_t s = 0; s < stars.size(); ++s) {
+		vec2 star = conv3Dto2D(stars[s]);
+		PutPixelSDL(screen, (int) star.x, (int) star.y, color);
+	}
+	if (SDL_MUSTLOCK(screen))
+		SDL_UnlockSurface(screen);
+	SDL_UpdateRect(screen, 0, 0, 0, 0);
+}
+
+void update() {
+
 }
